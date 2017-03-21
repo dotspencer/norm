7d6524ee7aa8c74aed582cf2bdfa7e9385725b32
Spencer Smith <dontaskspencer@gmail.com>
2017-03-21 01:07:38 -0600

    Added nav and pages

--- app.js
+++ app.js
@@ -7,6 +7,26 @@ app.use(express.static('public'));
 app.set('views', 'src/views');
 app.set('view engine', 'ejs');
 
+/*=======
+  Routes
+========*/
+
 app.get('/', (req, res) => {
   res.render('homepage');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+app.get('/settings', (req, res) => {
+  res.render('settings');
+});
+
+app.get('/send', (req, res) => {
+  res.render('settings');
 });
