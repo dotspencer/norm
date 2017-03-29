827bfa0784cd7418e192a7ab50d94527723ef3d8
Spencer Smith <dontaskspencer@gmail.com>
2017-03-28 22:29:15 -0600

    Session store with the express-mysql-session

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 
 var keys = require('./src/config/keys.json');
 var router = require('./src/config/routes.js');
-var authMiddleware = require('./src/middleware/authMiddleware.js')
+var authMiddleware = require('./src/middleware/authMiddleware.js');
 
 var app = express();
 app.listen('2323');
@@ -17,19 +18,6 @@ app.use(expressLayouts);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-var sessionOptions = {
-  secret: keys.secret,
-  resave: true,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 1209600000
-  }
-}
-app.use(session(sessionOptions));
-
-app.set('views', 'src/views');
-app.set('view engine', 'ejs');
-
 // Database
 var mysql = require('mysql');
 app.locals.db = mysql.createConnection({
@@ -40,11 +28,35 @@ app.locals.db = mysql.createConnection({
 });
 app.locals.db.connect();
 
-// Custom middleware
+// Sessions
+var sessionOptions = {
+  secret: keys.secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1209600000
+  }
+}
+app.use(session(sessionOptions));
+
+// Session Store
+var options = {
+    host: 'localhost',
+    port: 3306,
+    user: keys.mysql.username,
+    password: keys.mysql.password,
+    database: 'norm'
+};
+var connection = mysql.createConnection(options); // or mysql.createPool(options);
+var sessionStore = new MySQLStore({}/* session store options */, connection);
+
+// Authentication
 app.use(authMiddleware);
 
 // Helpers
 app.locals.staticHelper = require('./src/helpers/staticHelper.js');
 
 // Routes
+app.set('views', 'src/views');
+app.set('view engine', 'ejs');
 router(app);
