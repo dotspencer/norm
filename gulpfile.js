7b08dc09ffb106f7d27b8f265d37fa6fcd6a0a6d
Spencer Smith <dontaskspencer@gmail.com>
2017-04-01 14:54:34 -0600

    Fixed gulpfile to run tasks on any scss update

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,15 @@ var sass = require('gulp-sass');
 var nodemon = require('gulp-nodemon');
 var concat = require('gulp-concat');
 
+var generalInput = './public/scss/*.scss';
 var mainInput = './public/scss/main/*.scss';
 var dashInput = './public/scss/dashboard/*.scss';
 var output = './public/css';
 
-gulp.task('default', ['sass_main', 'sass_dash', 'watch_main', 'watch_dash', 'nodemon']);
+gulp.task('default', ['sass_main', 'sass_dash', 'watch', 'nodemon']);
 
-gulp.task('watch_main', function(){
-  return gulp.watch(mainInput, ['sass_main'])
-    .on('change', function(event){
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
-});
-
-gulp.task('watch_dash', function(){
-  return gulp.watch(dashInput, ['sass_dash'])
+gulp.task('watch', function(){
+  return gulp.watch(generalInput, ['sass_main', 'sass_dash'])
     .on('change', function(event){
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
