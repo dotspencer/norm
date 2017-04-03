6a674c2e8f0e092abb10726aa9fc7fae4375bcd4
Spencer Smith <dontaskspencer@gmail.com>
2017-04-03 13:03:22 -0600

    Added token column to Users table

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,5 +1,32 @@
+var keys = require('../config/keys.json');
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: keys.gmail.username,
+        pass: keys.gmail.password
+    }
+});
+
 function sendVerification(email){
-  console.log("Verify: " + email);
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Review Norm" <reviewnorm@gmail.com>', // sender address
+    to: email, // list of receivers
+    subject: 'Verify Review Norm Account', // Subject line
+    // text: 'Hello user, this is a test.', // plain text body
+    html: '<b>Hello user, this is a test.</b>' // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+    transporter.close();
+  });
 }
 
 
--- src/db/create_user_table.sql
+++ src/db/create_user_table.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS Users (
   email VARCHAR(120) NOT NULL UNIQUE,
   name VARCHAR(60) NOT NULL,
   hash VARCHAR(60) NOT NULL,
+  token VARCHAR(60) NOT NULL,
   verified BOOL NOT NULL DEFAULT 0,
   admin BOOL NOT NULL DEFAULT 0,
-  google_place_id VARCHAR(60) 
+  google_place_id VARCHAR(60)
 );
