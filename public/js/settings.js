b3215072c30025da74a6fd1714a2f4a0a0c5222e
Spencer Smith <dontaskspencer@gmail.com>
2017-04-03 20:52:40 -0600

    Load info into settings page forms

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,1 +1,19 @@
-function 
+var placeID = document.getElementById('place-id');
+var nameInput = document.getElementById('name-input');
+var emailInput = document.getElementById('email-input');
+
+function loadInfo(){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var result = JSON.parse(this.responseText);
+      nameInput.value = result.name;
+      emailInput.value = result.email;
+      placeID.value = result.placeID;
+    }
+  };
+  xhr.open('GET', '/api/user_info');
+  xhr.send();
+}
+
+loadInfo();
